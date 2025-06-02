7f65051bfdeaf7a2d521afb481d93a935e10c59e
Aleksandr Anosov <you@example.com>
2025-06-02 14:38:06 +0300

    separate positive and negative tests

---
+++ src/test/java/User/actions/positive/CreateUserPositiveTest.java
@@ -0,0 +1,38 @@
+package User.actions;
+
+import dto.NewUserDTO;
+import org.junit.jupiter.api.Test;
+import services.PetStoreApi;
+
+
+
+import static org.hamcrest.Matchers.equalTo;
+
+public class CreateUserPositiveTest {
+
+    /**
+     * Проверяет успешное создание нового пользователя с заданным ID.
+     * Ожидается, что API вернёт код 200 и в теле ответа message будет содержать переданный ID.
+     */
+
+    @Test
+    void createUser() {
+        long expectedMessage = 408L;
+        PetStoreApi api = new PetStoreApi();
+
+        NewUserDTO userDTO = NewUserDTO.builder()
+                .id(expectedMessage)
+                .firstName("SomeUser")
+                .userStatus(65L)
+                .phone("89-9090909")
+                .username("Kolya")
+                .build();
+
+        api.creteNewUser(userDTO)
+                .statusCode(200)
+                .body("message", equalTo(String.valueOf(expectedMessage)));
+
+    }
+
+
+}
